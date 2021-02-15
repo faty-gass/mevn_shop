dd6503b55e97471ee096fa1a28bc08d35216d231
Fatymata <fatymata.gassama@gmail.com>
2021-02-15 16:05:02 +0100

    [ADDED] connection to mongoDB & router file

---
+++ api/connection.js
@@ -0,0 +1,15 @@
+//Import the mongoose module
+const mongoose = require('mongoose');
+
+async function main(){
+  const mongoDB = 'mongodb+srv://dbuser:dbpassword@cluster0.lgimg.mongodb.net/myshop?retryWrites=true&w=majority';
+  try {
+    await mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log("Connected to mongo!")
+  }catch (e){
+    console.error(e)
+  }
+}
+
+
+module.exports = main;
---
+++ api/server.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = require('./routes')
+const db = require('./connection')
+
+const app = express()
+const port = 8080
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use('/api', router)
+
+db();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
