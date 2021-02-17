5ed98a73aa6b9bf34452220be24da53a2543ef01
Fatymata <fatymata.gassama@gmail.com>
2021-02-17 19:15:31 +0100

    [ADDED] add to cart button on product

--- app/src/components/Product.vue
+++ app/src/components/Product.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
-    <q-card class="my-card q-ma-md" flat bordered>
-      <q-card-section horizontal class="row items-center">
+    <q-card class="my-card q-ma-md q-pa-md" flat bordered>
+      <q-card-section class="row items-center">
         <q-card-section class="col-3 flex flex-center">
           <q-img class="rounded-borders " :src="product.image" alt="img_prod" />
         </q-card-section>
@@ -9,14 +9,22 @@
         <q-card-section
           class="q-pt-xs col-9 column justify-between text-center"
         >
-          <div class=" text-h5 q-mt-sm q-mb-xs text-center col col-md-4">
-            {{ product.name }}
+          <div class="col col-md-2 row justify-between">
+            <div class="col-10 text-h5 text-center ">
+              {{ product.name }}
+            </div>
+            <div class=" col-2">{{ product.price }} €</div>
           </div>
-          <div class=" text-caption text-grey col-auto">
+          <div class=" text-grey col-auto">
             {{ product.description }}
           </div>
-          <div class="text-overline text-right col col-md-4">
-            {{ product.price }} €€
+
+          <div class="col-md-2 text-right q-pt-md">
+            <q-btn
+              color="secondary"
+              icon-right="add_shopping_cart"
+              label="Add to cart"
+            />
           </div>
         </q-card-section>
       </q-card-section>
